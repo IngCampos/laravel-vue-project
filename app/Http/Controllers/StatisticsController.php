3d3e3b7345e714c7d7c93e1edc63e393f9a3ca03
IngCampos <15151130@aguascalientes.tecnm.mx>
2020-08-18 14:47:34 -0500

    Routes and details
    Routes for the views and controllers. Many have middleware for security.

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -16,7 +16,7 @@ use Carbon\Carbon;
 
 class StatisticsController extends Controller
 {
-    public function detailsusers()
+    public function details_user()
     {
         $data['total'] = User::all()->count();
         foreach (Department::all() as $department) {
@@ -25,7 +25,7 @@ class StatisticsController extends Controller
         return $data;
     }
 
-    public function detailspermissions()
+    public function details_permission()
     {
         $data['total'] = 0;
         foreach (Permission::all() as $permission) {
@@ -35,7 +35,7 @@ class StatisticsController extends Controller
         return $data;
     }
 
-    public function detailscomplaints($dates)
+    public function details_complaint($dates)
     {
         $date = explode(',', $dates);
         $data['total'] = Complaint::whereBetween('created_at', [$date[0], $date[1] . ' 23:59:59'])->count();
@@ -47,7 +47,7 @@ class StatisticsController extends Controller
         return $data;
     }
 
-    public function detailsmachines()
+    public function details_machine()
     {
         $data['total'] = Machine::all()->count();
         $data['machine_states'] = [];
@@ -58,14 +58,14 @@ class StatisticsController extends Controller
         return $data;
     }
 
-    public function detailsadvertisement()
+    public function details_advertisement()
     {
         $data['total'] = Advertisement::all()->count();
         $data['with_link'] = Advertisement::where('link', '!=', '')->count();
         return $data;
     }
 
-    public function detailstenders()
+    public function details_tender()
     {
         $data['total'] = Tender::all()->count();
         $data['internal_file'] = Tender::where('internal_file', 1)->count();
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,95 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Routes for people in general
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware(['auth', 'is.enabled']);
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Routes that need authentication
+Route::prefix('admin')->middleware(['auth', 'is.enabled'])->group(
+    function () {
+        // Routes in general for any user authenticated
+        Route::view('settings', 'userconfig')->name('userconfig');
+        Route::post('/update_password', 'UserConfigController@updatepassword')->name('update_password');
+
+        //Routes for data in forms
+        Route::get('api/data_user', 'DataFormController@data_user');
+        Route::get('api/data_department', 'DataFormController@data_department');
+        Route::get('api/data_dates_complaint', 'DataFormController@data_dates_complaint');
+        Route::get('api/data_tender_section', 'DataFormController@data_tender_section');
+        Route::get('api/date', 'DataFormController@date');
+
+        //Routes for data by ID
+        Route::get('api/user_id/{id}', 'DataByIdController@user_id');
+        Route::get('api/department_user_id/{id}', 'DataByIdController@department_user_id');
+
+        // Routes for System Administrator
+        Route::middleware(['has.permission:1'])->group(
+            function () {
+                $route = "/admin/";
+                Route::view('permissions', $route . 'permissions')->name('permissions');
+                Route::view('users', $route . 'user')->name('users');
+                Route::view('statistics/system', $route . 'statistics_system')->name('statistics_system');
+                Route::view('statistics/business', $route . 'statistics_website')->name('statistics_business');
+                Route::get('details/user', 'StatisticsController@details_user');
+                Route::get('details/permission', 'StatisticsController@details_permission');
+                Route::get('details/complaint/{dates}', 'StatisticsController@details_complaint');
+                Route::get('details/machine', 'StatisticsController@details_machine');
+                Route::get('details/advertisement', 'StatisticsController@details_advertisement');
+                Route::get('details/tender', 'StatisticsController@details_tender');
+                Route::resource('api/user', 'UserController')->except([
+                    'show', 'edit', 'create'
+                ]);
+                Route::resource('api/permission', 'PermissionController')->except([
+                    'update', 'edit', 'create'
+                ]);
+            }
+        );
+        // Routes for Complaint Administrator.
+        Route::middleware(['has.permission:2'])->group(
+            function () {
+                Route::view('complaints', 'complaints')->name('complaints');
+                Route::resource('api/complaint', 'ComplaintController')->only([
+                    'index', 'destroy'
+                ]);
+            }
+        );
+        // Routes for Advertisement Manager.
+        Route::middleware(['has.permission:3'])->group(
+            function () {
+                Route::view('advertisements', 'advertisements')->name('advertisements');
+                Route::resource('api/advertisement', 'AdvertisementController')->except([
+                    'show', 'edit', 'create'
+                ]);
+            }
+        );
+        // Routes for Machine Monitor.
+        Route::middleware(['has.permission:4'])->group(
+            function () {
+                Route::view('machines', 'machines')->name('machines');
+                Route::resource('api/machine', 'MachineStateController')->only([
+                    'index', 'update'
+                ]);
+            }
+        );
+        // Routes for Tender Administrator.
+        Route::middleware(['has.permission:5'])->group(
+            function () {
+                Route::view('tenders', 'tenders')->name('tenders');
+                Route::resource('api/tender', 'TenderController')->except([
+                    'index', 'edit', 'create'
+                ]);
+                Route::resource('api/tender_section', 'TenderSectionController')->except([
+                    'show', 'edit', 'create'
+                ]);
+            }
+        );
+    }
+);
+
+Route::view('user_disabled', 'user_disabled')->middleware(['auth', 'is.not.enable']);
+Route::post('/update_password', 'UserConfigController@updatepassword')->name('update_password')
+    ->middleware(['auth', 'is.enable']);
